058d23187e4f671471a93848b12403a11070fdc1
tsverova <tsverova.u.s@effective-group.ru>
2018-02-09 18:07:19 +0300

    init

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void MyFirstTest(){
+        driver.get("https://www.google.com/");
+
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver=null;
+    }
+
+
+
+
+
+}
+
