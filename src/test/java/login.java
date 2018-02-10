98c8294d72a566711037da1cb4ce6d7373b6fb83
Popov Aleksandr <palexashkav@gmail.com>
2018-02-10 16:33:10 +0400

    add login

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -3,12 +3,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyFirstTest {
-
+public class login {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -20,7 +18,12 @@ public class MyFirstTest {
 
     @Test
     public void MyFirstTest(){
-        driver.get("http://www.google.com");
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("developer");
+        driver.findElement(By.name("password")).sendKeys("developer");
+        driver.findElement(By.name("login")).click();
+
+
     }
 
     @After
@@ -29,9 +32,4 @@ public class MyFirstTest {
         driver=null;
     }
 
-
-
-
-
 }
-
