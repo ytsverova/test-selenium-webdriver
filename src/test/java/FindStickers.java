43b7a64c97b74356f6e4ca103d1341bc2427ae15
tsverova <tsverova.u.s@effective-group.ru>
2018-02-20 16:34:19 +0300

    init

---
+++ src/test/java/FindStickers.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class FindStickers {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void findStickers(){
+            driver.get("http://localhost/litecart/");
+            List<WebElement> products = driver.findElements(By.cssSelector("a.link"));
+            for (int i=1; i<products.size();i=i+1){
+                int count=products.get(i).findElements(By.cssSelector("div[class='sticker sale']")).size();
+                count= count+products.get(i).findElements(By.cssSelector("div[class='sticker new']")).size();
+                if (count!=1) {
+                    System.out.println("стикер не единтсвенный");
+                }
+
+            }
+        }
+
+
+
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver=null;
+    }
+}
